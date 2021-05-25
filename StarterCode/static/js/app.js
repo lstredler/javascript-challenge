34f6395618b9747ece77e1a3d78920065bd44bf9
Streds <laurenstredler@MacBook-Pro.local>
2021-05-25 08:26:14 -0500

    updates

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,20 +7,23 @@ var tableData = data;
 //column for date/time, city, state, country, shape, comment
 
 //button select 
-var $tbody= d3.select("tbody"); 
+var tbody = d3.select("tbody");
+console.log(data);
 var button = d3.select("#button");
+var cityInput = d3.select("#city")
+var stateInput = d3.select("#state");
+var countryInput = d3.select("#country")
+var shapeInput = d3.select("#shape")
+
+
 var inputFieldDate = d3.select("#datetime");
-var $stateInput = d3.select("#state");
 var $filerBtn = d3.select("#filter-btn");
-var $cityInput = d3.select("#city")
-var $countryInput = dd3.select("#country")
-var $shapeInput = d3.select("#shape")
 
 var columns = ["date/time", "city", "state", "country", "shape", "comment"]
 
 var addData = (dataInput) => {
     dataInput.forEach(ufoSightings =>{
-        var row = $body.append("tr");
+        var row = tbody.append("tr");
         columns.forEach(column => row.append("td").text(ufoSightings[column]))
     })
 }
