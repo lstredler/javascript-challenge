a1a75747692738c946f600b078b44dab75d704de
Streds <laurenstredler@MacBook-Pro.local>
2021-05-25 08:12:47 -0500

    updates

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,29 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// grab references to the input element and the output div
+//write code that appends a table to your web page 
+//and then adds new rows of data for each UFO sighting.
+//column for date/time, city, state, country, shape, comment
+
+//button select 
+var button = d3.select("#button");
+
+var $tbody= d3.select("tbody"); 
+
+var $dateInput = d3.select("#datetime");
+
+var $stateInput = d3.select("#state");
+
+var $filerBtn = d3.select("#filter-btn");
+
+var $cityInput = d3.select("#city")
+
+var $countryInput = dd3.select("#country")
+
+var $shapeInput = d3.select("#shape")
+
+//renderTable 
+funtion renderTable() {
+
+}
