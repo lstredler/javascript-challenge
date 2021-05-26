219fc4fa2f4ad85e0695b1414bd367ecb9747c7a
Streds <laurenstredler@MacBook-Pro.local>
2021-05-26 15:39:02 -0500

    updates

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -5,6 +5,7 @@ var tableData = data;
 //write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
 
 var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
 var datetimeInput = d3.select("#datetime");
 var cityInput = d3.select("#city");
 var stateInput = d3.select("#state");
@@ -13,14 +14,10 @@ var shapeInput = d3.select("#shape");
 var durationMinutesInput = d3.select("#durationMinutes");
 var comments = d3.select("#comments");
 
-// Print the value to the console
-console.log(data);
-
-var filerBtn = d3.select("#filter-btn");
-
 //column for date/time, city, state, country, shape, comment
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
+//data to html 
 var addData = (dataInput) => {
     dataInput.forEach(ufoSightings =>{
         var row = tbody.append("tr");
@@ -28,6 +25,11 @@ var addData = (dataInput) => {
     })
 }
 addData(tableData); 
+
+// Print the value to the console
+console.log(data);
+
+var filerBtn = d3.select("#filter-btn");
 
 //prevent table from reloading
 d3.event.preventDefault();
